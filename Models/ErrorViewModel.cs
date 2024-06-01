6f89b5d7e1df0c7d088d4bc4d1fd63550357290e
Eric Selover <eric.selover@gmail.com>
2024-06-01 16:31:31 -0500

    feat: init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pearson_CodingChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
